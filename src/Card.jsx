b5fc79e59dc04fbe17e63b2cbc73bb2d8fd543ee
deadpool <118651379+DocStrange31@users.noreply.github.com>
2025-02-28 00:16:13 +0530

    feature: Added parallax tilt effect to cards set max height to viewport height for mobile displays

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,27 +1,37 @@
 /* eslint-disable react/prop-types */
+import Tilt from "react-parallax-tilt";
 import friends_logo from "./assets/friends_logo.png";
 
 export default function Card({ name, image, onClick, flipped }) {
   return (
-    <div className="card-container">
-      <div
-        className={`card ${flipped ? "flipped" : ""}`}
-        onClick={
-          flipped
-            ? null
-            : () => {
-                onClick(name);
-              }
-        }
-      >
-        <div className="card-face card-front">
-          <img src={image} alt="Character Image" />
-          <div className="name">{name}</div>
-        </div>
-        <div className="card-face card-back">
-          <img src={friends_logo} alt="" />
+    <Tilt
+      glareEnable={true}
+      tiltReverse={true}
+      glareReverse={true}
+      glareMaxOpacity={0.4}
+      tiltMaxAngleX={20}
+      tiltMaxAngleY={20}
+    >
+      <div className="card-container">
+        <div
+          className={`card ${flipped ? "flipped" : ""}`}
+          onClick={
+            flipped
+              ? null
+              : () => {
+                  onClick(name);
+                }
+          }
+        >
+          <div className="card-face card-front">
+            <img src={image} alt="Character Image" />
+            <div className="name">{name}</div>
+          </div>
+          <div className="card-face card-back">
+            <img src={friends_logo} alt="" />
+          </div>
         </div>
       </div>
-    </div>
+    </Tilt>
   );
 }
